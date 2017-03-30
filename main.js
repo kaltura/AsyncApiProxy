050b7f03dd0c199b9fd9d9071a504414811d6ce4
gotlieb <gotlieb.erez@gmail.com>
2017-03-30 12:01:06 +0300

    PLAT-7198 creating async repo and handling config

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-var AsyncProxyManager = require('./lib/AsyncProxyManager');
-const continuationLocalStorage = require('continuation-local-storage');
-let config = require('config');
 require('./lib/utils/KalturaLogger');
+const AsyncProxyManager = require('./lib/AsyncProxyManager');
+const continuationLocalStorage = require('continuation-local-storage');
+const KalturaAsyncProxyServerValidator = require('./lib/utils/KalturaAsyncProxyServerValidator');
 
 function KalturaMainProcess(){
-	this.start();
+this.start();
 };
 
 KalturaMainProcess.prototype.start = function () {
 
 	this.namespace = continuationLocalStorage.createNamespace('async-proxy-server');//Here just to make sure we create it only once
-	var version = config.get('server.version');
-	KalturaLogger.log('\n\n_____________________________________________________________________________________________');
-	KalturaLogger.log('Async-Proxy-Server ' + version + ' started');
-	
-	var conn = new AsyncProxyManager();
+	var server = new AsyncProxyManager();
+	server.startServer();
+
 	process.on('SIGUSR1', function() {
-                KalturaLogger.log('Got SIGUSR1. Invoke log rotate notification.');
-                KalturaLogger.notifyLogsRotate();
-        });
+		KalturaLogger.log('Got SIGUSR1. Invoke log rotate notification.');
+		KalturaLogger.notifyLogsRotate();
+	});
 
 };
 
